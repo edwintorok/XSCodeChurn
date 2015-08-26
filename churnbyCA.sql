ff7bccf13a62c5cedd5839981cd19fad782ba3e8
philippe gabriel <philippe.gabriel@citrix.com>
2015-08-26 16:36:56 +0000

    churn query now joined with filemap table

--- churnbyCA.sql
+++ churnbyCA.sql
@@ -1,7 +1,10 @@
 -- Filenames changed by CAs
 -- with total code churn
 -- ranked by number of CAs involved
-select filename,count(uuid) as count,sum(churn) as total from filechurn where uuid in 
+select filechurn.filename,count(filechurn.uuid) as count,sum(filechurn.churn) as total,filemap.loc
+from filechurn  
+full join filemap on filechurn.filename = filemap.filename
+where filechurn.uuid in 
 	(select uuid from commit where jiratype='CA')
-group by filename 
+group by filechurn.filename,filemap.loc
 order by count desc;
