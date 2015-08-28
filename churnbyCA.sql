67becfa52d856718d4daed8222d08511bac0d7dc
philippe gabriel <philippe.gabriel@citrix.com>
2015-08-28 15:33:06 +0000

    adding a percent column to churnbyCA query

--- churnbyCA.sql
+++ churnbyCA.sql
@@ -1,10 +1,10 @@
 -- Filenames changed by CAs
 -- with total code churn
 -- ranked by number of CAs involved
-select filechurn.filename,filechurn.repo,count(filechurn.uuid) as count,sum(filechurn.churn) as total,filemap.loc
-from filechurn,filemap  
-where filechurn.uuid in 
+select f.filename,f.repo,count(f.uuid) as count,sum(f.churn) as total,m.loc as loc,round(sum(f.churn)*100/m.loc) as percent
+from filechurn f,filemap m  
+where f.uuid in 
 	(select  uuid from commit where jiratype='CA')
-and filechurn.repo=filemap.repo and filechurn.filename=filemap.filename
-group by filechurn.repo,filechurn.filename,filemap.loc
+and f.repo=m.repo and f.filename=m.filename
+group by f.repo,f.filename,m.loc
 order by count desc;
