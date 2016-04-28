5e33b18c98f9daf9918baed09e1895f76a8de20b
philippe gabriel <philippe.gabriel@citrix.com>
2016-04-28 18:19:02 +0100

    simplified churnByReposByDate.sql with a with clause

--- churnByReposByDate.sql
+++ churnByReposByDate.sql
@@ -1,8 +1,9 @@
 -- ranked by number of CAs involved
 .print <caption>#commits & churn by repos by date</caption>
+with g as (select * from gitcommit c inner join filechurn f on f.uuid=c.uuid where c.repo='xen-api.git')
 select g.date as "date", g.repo as "repo",count(distinct g.uuid) as "#total commits",count(distinct j.jiraid) as "#CA commits",sum(g.churn) as "churn",sum(j.churn) as "CA churn"
-from (select * from gitcommit c inner join filechurn f on f.uuid=c.uuid where c.repo='xen-api.git') g
-left outer join (select c.uuid,c.jiraid,f.churn from gitcommit c inner join filechurn f on f.uuid=c.uuid where c.repo='xen-api.git' and c.jiratype='CA') j
+from  g
+left outer join (select uuid,jiraid,churn from g where repo='xen-api.git' and jiratype='CA') j
 on g.uuid=j.uuid
 group by g.date
 order by g.date desc;
