bc2e026ee1701f7b7ddc2273f8a3ea34cc559006
philippe gabriel <philippe.gabriel@citrix.com>
2016-04-30 16:23:38 +0100

    Fixed churnByReposByDate.sql

--- churnByReposByDate.sql
+++ churnByReposByDate.sql
@@ -1,9 +1,8 @@
 -- ranked by number of CAs involved
 .print <caption>#commits & churn by repos by date</caption>
-with g as (select * from gitcommit c inner join filechurn f on f.uuid=c.uuid where c.repo='xen-api.git')
-select strftime('%Y-%m', g.date) as "month",g.date as "date", g.repo as "repo",count(distinct g.uuid) as "#total commits",count(distinct j.jiraid) as "#CAs",sum(g.churn) as "total LOC churn",sum(j.churn) as "CA LOC churn"
-from  g
-left outer join (select uuid,jiraid,churn from g where repo='xen-api.git' and jiratype='CA') j
+with g as (select strftime('%Y-%m', c.date) as month,c.uuid,c.repo,c.date,c.jiratype,c.jiraid,sum(f.churn) as sumchurn from gitcommit c inner join filechurn f on f.uuid=c.uuid where c.repo='xen-api.git' group by c.uuid)
+select g.month,g.uuid,g.repo,g.date,g.jiratype,g.jiraid,g.sumchurn,j.sumchurn from  g
+left outer join (select uuid,sumchurn from g where jiratype='CA') j
 on g.uuid=j.uuid
-group by g.date
+-- group by g.date
 order by g.date desc;
