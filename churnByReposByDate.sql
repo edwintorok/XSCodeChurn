a271e40f1b99ed31a773451bc55f76ffdf1a9b4d
philippe gabriel <philippe.gabriel@citrix.com>
2016-04-30 15:23:17 +0100

    minor fix

--- churnByReposByDate.sql
+++ churnByReposByDate.sql
@@ -1,7 +1,7 @@
 -- ranked by number of CAs involved
 .print <caption>#commits & churn by repos by date</caption>
 with g as (select * from gitcommit c inner join filechurn f on f.uuid=c.uuid where c.repo='xen-api.git')
-select g.date as "date", g.repo as "repo",count(distinct g.uuid) as "#total commits",count(distinct j.jiraid) as "#CA commits",sum(g.churn) as "churn",sum(j.churn) as "CA churn"
+select strftime('%Y-%m', g.date) as "month",g.date as "date", g.repo as "repo",count(distinct g.uuid) as "#total commits",count(distinct j.jiraid) as "#CAs",sum(g.churn) as "total LOC churn",sum(j.churn) as "CA LOC churn"
 from  g
 left outer join (select uuid,jiraid,churn from g where repo='xen-api.git' and jiratype='CA') j
 on g.uuid=j.uuid
