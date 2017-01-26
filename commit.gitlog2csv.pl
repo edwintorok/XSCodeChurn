3f7176af33f34887bd83d91c93838292ed754a4a
philippe gabriel <philippe.gabriel@citrix.com>
2017-01-26 12:46:03 +0000

    Fixed regex bug

--- commit.gitlog2csv.pl
+++ commit.gitlog2csv.pl
@@ -6,13 +6,13 @@ while(<>){
 #skip blank lines
 	/^\s*$/ and next;
 #strip non ascii chars
-	s/[[:^ascii:]]//g;
+	s/[[:^ascii:]]/!/g;
 #strip all chars which trip the csv psql import
-	s/["',]//g;
+	s/["',]/;/g;
 #extract jira object
 	my ($uuid,$repo,$author,$date,$summary)=split(/\t/);
 	my ($jiratype,$jiraid)=('###',0);
-	if($summary =~ /^.*?(CA|CP|XOP|SCTX|HFX|HFP|CAR)-([0-9]*).*?$/i){
+	if($summary =~ /^.*?(CA|CP|XOP|SCTX|HFX|HFP|CAR)-([0-9]+).*?$/i){
 		($jiratype,$jiraid)=(uc $1,$2);}
 	my $tmp=substr $summary,0,80;
 	print "$uuid,$repo,$author,$date,$jiratype,$jiraid,$tmp\n";
